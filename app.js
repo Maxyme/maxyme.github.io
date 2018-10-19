a8f3b43602d5f640aa65de1c040c2121afe3827e
maxyme <maxyme@outlook.com>
2018-10-19 14:59:45 -0400

    small modifications

--- app.js
+++ app.js
@@ -1,53 +1,19 @@
 (function() {
-    var app = {
-      'routes': {
-        'blog': {
-          'rendered': function() {
-            app.preventScroll();
-          }
-        },
-        'projects': {
-          'rendered': function() {
-            app.preventScroll();
-          }
-        },
-        'hobbies': {
-          'rendered': function() {
-            app.preventScroll();
-          }
-        },
-        'contact': {
-          'rendered': function() {
-            app.preventScroll();
-          }
-        },
-        'the-default-view': {
-          'rendered': function() {
-            app.preventScroll();
-          }
-        },
-      },
-      'default': 'the-default-view',
-      'preventScroll': function() {
-        document.querySelector('html').scrollTop = 0;
-        document.querySelector('body').scrollTop = 0;
-      },
-      'routeChange': function() {
-        app.routeID = location.hash.slice(1);
-        app.route = app.routes[app.routeID];
-        app.routeElem = document.getElementById(app.routeID);
-        app.route.rendered();
-      },
-      'init': function() {
-        window.addEventListener('hashchange', function() {
-          app.routeChange();
-        });
-        if (!window.location.hash) {
-          window.location.hash = app.default;
-        } else {
-          app.routeChange();
+    var preventScroll = {
+        'rendered': function() {
+          document.querySelector('html').scrollTop = 0;
+          document.querySelector('body').scrollTop = 0;
         }
       }
+    var app = {
+      'routes': {
+        'blog': preventScroll,
+        'projects': preventScroll,
+        'hobbies': preventScroll,
+        'contact': preventScroll,
+        'home': preventScroll,
+      },
+      'default': 'home',
     };
     window.app = app;
   })();
